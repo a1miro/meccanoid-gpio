cc61a8bb05446f197fd7ac6d9900066588c8b46a
Andrei Mironenko <amironenko@amironenko-ubuntu2.hanover.local>
2017-04-03 21:59:23 +0100

    Starting with the python module development

---
+++ python/meccanoid/meccabrain.py
@@ -0,0 +1,17 @@
+class meccabrain(object):
+    
+
+    def communicate(self):
+        pass
+
+    def send(self, byte):
+        pass
+
+    def receive(self):
+        pass
+
+    def checksum(self, data1, data2, data3, data4):
+        pass
+
+
+
