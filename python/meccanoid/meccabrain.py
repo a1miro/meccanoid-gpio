d1dcf7b8779c0a3c95acebfa4686059cef4a8ce6
Andrei Mironenko <andrei.mironenko@gmail.com>
2017-05-17 23:08:30 +0100

    First, but not working version ...

--- python/meccanoid/meccabrain.py
+++ python/meccanoid/meccabrain.py
@@ -21,26 +21,54 @@ class MeccaBrain(object):
     @property
     def pin(self):
         return self._pin
+
+
+    def color(self, servo, color):
+        self._output[servo] = color
     
 
-    def _communicate(self):
+    def communicate(self):
         '''
         communicate()  -  this is the main method that takes care of 
         initializing, sending data to and receiving data from Meccano Smart
         modules
 
-        The datastream consists of 6 output bytes sent to the Smart modules   and  one return input byte from the Smart modules.  Since there can be a maximum of 4 Smart modules in a chain, each module takes turns replying along the single data wire.
-
+        The datastream consists of 6 output bytes sent to the Smart modules
+        and  one return input byte from the Smart modules.  Since there can
+        be a maximum of 4 Smart modules in a chain, each module takes turns
+        replying along the single data wire.  
+        
         Output bytes:   
         0xFF - the first byte is always a header byte of 0xFF
-        Data 1 -  the second byte is the data for the Smart module at the 1st position in the chain
-        Data 2 -  the third byte is the data for the Smart module at the 2nd position in the chain
-        Data 3 -  the fourth byte is the data for the Smart module at the 3rd position in the chain
-        Data 4 -  the fifth byte is the data for the Smart module at the 4th position in the chain 
-        Checksum  -  the sixth byte is part checksum, part module ID.  The module ID tells which of the modules in the chain should reply end
+        Data 1 -  the second byte is the data for the Smart module at the 
+        1st position in the chain
+        Data 2 -  the third byte is the data for the Smart module at the 
+        2nd position in the chain
+        Data 3 -  the fourth byte is the data for the Smart module at the 
+        3rd position in the chain
+        Data 4 -  the fifth byte is the data for the Smart module at the 
+        4th position in the chain 
+        Checksum  -  the sixth byte is part checksum, part module ID.  
+        The module ID tells which of the modules in the chain should 
+        reply end
 
         '''
-        pass
+        self._send(0xFF)     # Send header
+
+        for b in self._output:
+            self._send(b)
+
+        checksum = self._checksum(
+                self._output[0], 
+                self._output[1], 
+                self._output[2], 
+                self._output[3]
+                )
+
+        self._send(checksum)  # Sending checksum
+        return self._receive()
+
+
 
     def _send(self, data):
 
@@ -48,7 +76,7 @@ class MeccaBrain(object):
 
         # Start bit, 417 usec LOW
         GPIO.output(self._pin, GPIO.LOW)
-        usleep(BIT_DELAY_USEC)
+        usleep(MeccaBrain.BIT_DELAY_USEC)
 
         for b in range(0,8):
             mask = 1 << b;
@@ -56,22 +84,19 @@ class MeccaBrain(object):
                 GPIO.output(self._pin, GPIO.HIGH)
             else:
                 GPIO.output(self._pin, GPIO.LOW)
-            usleep(BIT_DELAY_USEC)
+            usleep(MeccaBrain.BIT_DELAY_USEC)
 
         # Stop bit - HIGH 
         GPIO.output(self._pin, GPIO.LOW)
-        usleep(BIT_DELAY_USEC)
+        usleep(MeccaBrain.BIT_DELAY_USEC)
 
         # Stop bit - HIGH 
         GPIO.output(self._pin, GPIO.LOW)
-        uslepp(BIT_DELAY_USEC)
-        
-
-
+        usleep(MeccaBrain.BIT_DELAY_USEC)
 
     def _receive(self):
         data = 0
-        GPIO.setup(self._pin, GPIO.INPUT)
+        GPIO.setup(self._pin, GPIO.IN)
 
         # TODO: Why so long time 1.5?
         time.sleep(0.5)
@@ -83,12 +108,9 @@ class MeccaBrain(object):
                 pass
                 data |= 1 << b;
 
-            usleep(READ_BIT_DELAY_USEC)
+            usleep(MeccaBrain.READ_BIT_DELAY_USEC)
 
         return data
-
-
-
 
     def _checksum(self, data1, data2, data3, data4):
         CS = data1 + data2 + data3 + data4 # ignore overflow
@@ -99,4 +121,13 @@ class MeccaBrain(object):
         return CS
 
 
+
+if __name__ == "__main__":
+    mecca = MeccaBrain(21)
+    colors = [ 0xF7, 0xF6, 0xF5, 0xF4, 0xF3, 0xF2, 0xF1, 0xF0]
+    for c in colors:
+        mecca.color(1,c)
+        r = mecca.communicate()
+        print("return = %d" % r)
+        time.sleep(1)
 
